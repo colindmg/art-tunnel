e3e697b2a7091effff6654edaeb0a562dae9f313
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-09-09 00:18:17 +0200

    Retrait des éléments superflux et remise au clair des fichiers

--- src/main.js
+++ src/main.js
@@ -1,121 +1,12 @@
-import gsap from "gsap";
 import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { AfterimagePass } from "three/examples/jsm/postprocessing/AfterimagePass.js";
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
 import { OutputPass } from "three/examples/jsm/postprocessing/OutputPass.js";
 import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
-import artistList from "./data/artistList";
 import spiralFragmentShader from "./shaders/coffeeSmoke/fragment.glsl";
 import spiralVertexShader from "./shaders/coffeeSmoke/vertex.glsl";
 import "./style.css";
-
-// GESTION DE LA LISTE DES ARTISTES
-
-let selectedArtistIndex = 0;
-
-document.addEventListener("DOMContentLoaded", () => {
-  const artistListContainer = document.getElementById("artist-list");
-
-  // Function to create a div for each artist
-  artistList.forEach((artist, index) => {
-    const artistDiv = document.createElement("div");
-    artistDiv.classList.add(
-      "artist-item",
-      "cursor-pointer",
-      "hover:opacity-100"
-    );
-    artistDiv.style.display = "flex";
-    artistDiv.style.opacity = 0.5;
-    artistDiv.onpointerenter = () => {
-      artistDiv.style.opacity = 1;
-    };
-    artistDiv.onpointerleave = () => {
-      if (parseInt(artistDiv.dataset.index) !== selectedArtistIndex) {
-        artistDiv.style.opacity = 0.5;
-      }
-    };
-    // artistDiv.innerText = `${artist.name} (${artist.twitterAccount})`;
-    artistDiv.dataset.index = index;
-
-    // Create the span for the artist's name
-    const nameSpan = document.createElement("span");
-    nameSpan.style.width = "150px";
-    nameSpan.innerText = artist.name;
-
-    // Create the span for the artist's Twitter account
-    const twitterSpan = document.createElement("span");
-    twitterSpan.style.width = "150px";
-    twitterSpan.innerText = artist.twitterAccount;
-
-    // Append spans to the artistDiv
-    artistDiv.appendChild(nameSpan);
-    artistDiv.appendChild(twitterSpan);
-
-    if (index === selectedArtistIndex) {
-      artistDiv.style.opacity = 1;
-      artistDiv.classList.add("selected");
-    }
-
-    artistDiv.addEventListener("click", () => {
-      document.querySelectorAll(".artist-item").forEach((item) => {
-        item.style.opacity = 0.5;
-        item.classList.remove("selected");
-      });
-
-      artistDiv.style.opacity = 1;
-      artistDiv.classList.add("selected");
-      selectedArtistIndex = parseInt(artistDiv.dataset.index);
-    });
-
-    artistListContainer.appendChild(artistDiv);
-  });
-
-  // ANIMATION GSAP D'APPARITION DES ÉLÉMENTS HTML
-  gsap.from("#title1", {
-    duration: 0.5,
-    opacity: 0,
-    filter: "blur(10px)",
-    ease: "power2.out",
-  });
-
-  gsap.from("#title2", {
-    duration: 0.5,
-    opacity: 0,
-    filter: "blur(10px)",
-    ease: "power2.out",
-    delay: 0.25,
-  });
-
-  gsap.from("#titlebar", {
-    duration: 1,
-    width: 0,
-    marginLeft: 0,
-    marginRight: 0,
-    opacity: 0,
-    ease: "power2.inOut",
-    delay: 0.5,
-  });
-
-  gsap.from(".artist-item", {
-    duration: 1,
-    opacity: 0,
-    filter: "blur(10px)",
-    ease: "power2.out",
-    stagger: 0.08,
-    delay: 0.75,
-  });
-
-  gsap.to("#overlay", {
-    duration: 1.5,
-    opacity: 0,
-    backdropFilter: "blur(100px)",
-    ease: "power2.inOut",
-    delay: 1.25,
-  });
-});
-
-// ----------------------------------------------------
 
 // GESTION DE LA SCENE THREE.JS
 
@@ -134,6 +25,9 @@ scene.background = new THREE.Color(0xf6faf9);
 // Loaders
 const textureLoader = new THREE.TextureLoader();
 const gltfLoader = new GLTFLoader();
+
+const texture = textureLoader.load("/textures/rokleam.webp");
+texture.wrapT = THREE.RepeatWrapping;
 
 /**
  * Sizes
@@ -167,7 +61,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.x = -1;
+camera.position.x = 0;
 camera.position.y = 0;
 camera.position.z = 8;
 scene.add(camera);
@@ -228,7 +122,7 @@ const spiralMaterial = new THREE.ShaderMaterial({
   fragmentShader: spiralFragmentShader,
   uniforms: {
     uTime: new THREE.Uniform(0),
-    uPerlinTexture: new THREE.Uniform(artistList[selectedArtistIndex].texture),
+    uPerlinTexture: new THREE.Uniform(texture),
   },
 });
 
@@ -246,15 +140,6 @@ const tick = () => {
 
   // Update spiral
   spiral.material.uniforms.uTime.value = elapsedTime;
-
-  // Update texture
-  if (
-    artistList[selectedArtistIndex].texture !==
-    spiral.material.uniforms.uPerlinTexture.value
-  ) {
-    spiral.material.uniforms.uPerlinTexture.value =
-      artistList[selectedArtistIndex].texture;
-  }
 
   // Render
   renderer.render(scene, camera);
